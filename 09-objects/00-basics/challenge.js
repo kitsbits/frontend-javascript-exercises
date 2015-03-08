08e08fb3f058774ab9d1b45734d0e00cab4a252b
noblepaper <noblepapergraphics@gmail.com>
2015-03-08 00:47:07 -0500

    complete objects exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,37 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+		var createCourse = {};
+		createCourse.title = courseTitle;
+		createCourse.duration = courseDuration;
+		createCourse.students = courseStudents;
+		
+		return createCourse;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function (object, newProperty, newValue) {
+	if (!object[newProperty]) {
+		object[newProperty] = newValue;
+	}
+	return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+	return("Hello " + letter.recipient + "," + "\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender);
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function (item, money) {
+	var canAfford = {
+		item1: 'MacBook Air',
+		money1: 999,
+		item2: 'MacBook Pro',
+		money2: 1299,
+		item3: 'Mac Pro',
+		money3: 2499,
+        item4: 'Apple Sticker',
+        money4: 1
+
+	}
+	return((item === canAfford.item1 && money >= canAfford.money1) 
+		|| (item === canAfford.item2 && money >= canAfford.money2)
+		|| (item === canAfford.item3 && money >= canAfford.money3)
+		|| (item === canAfford.item4 && money >= canAfford.money4));
+};
