f6a365965b7065a7c1b2264cfc67abd9160b32cb
Kit <noblepapergraphics@gmail.com>
2015-03-09 17:22:23 -0400

    complete 02-obj-ops exercises; hasElems exercise question

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,30 @@
-module.exports.copy = undefined;
 
-module.exports.extend = undefined;
+module.exports.copy = function(object) {
+  var newObject = {};
+  for (var i in object) {
+    newObject[i] = object[i];
+  }
+  return newObject;
+};
 
-module.exports.hasElems = undefined;
+module.exports.extend = function(dest, src) {
+  for (var property in src) {
+    dest[property] = src[property];
+  }
+  return dest;
+};
+
+module.exports.hasElems = function(object, array) {
+  var keys = Object.keys(object);
+  //console.log(keys);
+  keys.sort();
+  array.sort();
+  for (i = 0; i < keys.length; i++) {
+    if (keys[i] !== array[i]) {
+      return false;
+    }
+    return true;
+  }
+
+  
+};
