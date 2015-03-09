2591e8260539976d97b8805ff5898bbbc7365c91
noblepaper <noblepapergraphics@gmail.com>
2015-03-09 15:16:05 -0400

    complete 2 of 4 loop exercises

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,25 @@
-module.exports.sumNumbers = undefined;
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.sumNumbers = function(array) {
+	var arraySum = 0;
+	for (var i = 0; i < array.length; i++) {
+		arraySum += array[i];	
+	}
+	return(arraySum);
+};
 
-module.exports.addIndex = undefined;
+module.exports.splitAndLowerCaseString = function(string) {
+	var lowerCaseString = string.toLowerCase();
+	var splitString = lowerCaseString.split(",");
+	return(splitString);
+};
+
+module.exports.addIndex = function(array) {
+	//console.log(array);
+	var newArray = [];
+	for (i = 0; i < array.length; i++) {
+		newArray.push(i + " is " + array[i]);
+		//console.log(newArray);
+	}
+	return(newArray);
+};
+
--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,29 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object) {
+	var keys = Object.keys(object);
+	return(keys);
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function (object) {
+	var array =[];
+	for (var values in object) {
+		array.push(object[values]);
+	}
+	return array;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object) {
+	var keys = Object.keys(object);
+	//console.log(keys);
+	var values = [];
+	for (var property in object) {
+		values.push(object[property]);
+	}
+	//console.log(values);
+	var finalArray = [];
+	for (i = 0; i < keys.length; i++) {
+		finalArray.push(keys[i] + " is " + values[i]);
+	}
+	//console.log(finalArray);
+	return finalArray;
+
+};
