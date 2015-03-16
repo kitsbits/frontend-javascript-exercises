755bac4cdb402987588f2e0d61f2f1ae0c5c086b
Kit <noblepapergraphics@gmail.com>
2015-03-16 17:51:50 -0400

    completed array exercises

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,14 +1,25 @@
 module.exports.stream = function(conditionalFn, actionFn) {
-    actionFn = function() {console.log("Hello!");};
-
-    var counter = 10;
-    while (counter <= 10) {
-      conditionalFn = function() { 
-      counter--;
-      return actionFn;
-      }
-    };
+  while ( conditionalFn() ) {
+    //executes this code
+    actionFn();
+  }
 };
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+  var numOfElements = 0;
+  var runningTotal = 0;
+  while (numOfElements <= array.length - 1) {
+    runningTotal += array[numOfElements];
+    numOfElements++;
+  }
 
+  return runningTotal;
+};
+
+// module.exports.sumNumbers = function(arr) {
+//   var sum = 0;
+//   while(arr.length > 0) {
+//     sum += arr.pop();
+//   }
+//   return sum;
+// };
